3bed99fad436c1ea32fe4a34ad1a4277e984e70b
il3ni4 <moonwalk341@gmail.com>
2024-09-19 20:24:36 +0200

    ooooo

--- server/index.js
+++ server/index.js
@@ -20,17 +20,6 @@ app.use(cors({
 app.use("/auth", userRoutes)
 app.use("/home", recipeRoutes)
 
-
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-
 mongoose.connect(process.env.MONGO_URL);
 const db = mongoose.connection;
 db.once("open", () => {console.log("Database connesso con successo")});
