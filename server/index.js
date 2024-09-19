be42aa64fced9713c00ea56e0b88764b62a1af19
il3ni4 <moonwalk341@gmail.com>
2024-09-19 20:22:07 +0200

    h

--- server/index.js
+++ server/index.js
@@ -5,18 +5,12 @@ const userRoutes = require("./routes/users");
 const recipeRoutes = require("./routes/recipes");
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const path = require('path');
 
 const app = express();
 
 app.use(express.json({limit: '30mb'}));
 app.use(express.urlencoded({limit: '25mb', extended: true}));
 app.use(cookieParser());
-
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 app.use(cors({
     origin: 'https://recipapp-frontend.onrender.com',
