4bde48a3f9df72490da7498dd64bc4a32ece6fbd
il3ni4 <moonwalk341@gmail.com>
2024-09-19 19:27:40 +0200

    homepage .

--- server/index.js
+++ server/index.js
@@ -7,18 +7,30 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors')
 
 const app = express();
+const path = require('path');
 
 app.use(express.json({limit: '30mb'}));
 app.use(express.urlencoded({limit: '25mb', extended: true}));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://recipapp-frontend.onrender.com',
     credentials: true
   }));
 
 app.use("/auth", userRoutes)
 app.use("/home", recipeRoutes)
+
+
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 mongoose.connect(process.env.MONGO_URL);
 const db = mongoose.connection;
