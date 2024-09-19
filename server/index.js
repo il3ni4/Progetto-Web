557ef988886069de8a548dda0eef704243915d09
il3ni4 <moonwalk341@gmail.com>
2024-09-19 20:18:10 +0200

    path

--- server/index.js
+++ server/index.js
@@ -8,7 +8,6 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const path = require('path');
 
 app.use(express.json({limit: '30mb'}));
 app.use(express.urlencoded({limit: '25mb', extended: true}));
